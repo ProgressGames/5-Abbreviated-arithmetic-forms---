33f61f7d1958d11f05c62ccf218540aa24a56a0e
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-08-17 20:47:22 +0300

    tips

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ void main()
 {
 	int A = 20;
 
-	A += 10;
+	A += 10; // это A = A + 10; | this is A = A + 10;
 
-	A -= 10;
+	A -= 10; // это A = A - 10; | this is A = A - 10;
 
-	A *= 10;
+	A *= 10; // это A = A * 10; | this is A = A * 10;
 
-	A /= 10;
+	A /= 10; // это A = A / 10; | this is A = A - 10;
 
 	cout << A << endl;
 }
